3fbf261a119e28c5ccd109ffa54c4399a8484518
Hatice-Altun <van.altun@outlook.com>
2022-11-04 13:58:58 +0100

    Conditional Statment Example

---
+++ src/com/altun/ConditionalStatement/Main.java
@@ -0,0 +1,13 @@
+package com.altun.ConditionalStatement;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// For loop example: Character by position!
+
+        for(int i = -128; i < 127; i++) {
+            char ch = (char) i;
+            System.out.println("Character by position: " + i + " is" + ch);
+        }
+    }
+}
